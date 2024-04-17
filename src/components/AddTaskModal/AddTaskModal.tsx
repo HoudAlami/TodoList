89fb053e31c64a2b3ff3922cbc23f4ebee4b6e0a
HoudAlami <houd.alami-marrouni@oclock.school>
2024-04-17 21:42:24 +0200

    open/close taskModal

--- src/components/AddTaskModal/AddTaskModal.tsx
+++ src/components/AddTaskModal/AddTaskModal.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './AddTaskModal.scss';
 
-function AddTaskModal() {
-  return <div>Hello</div>;
+type AddTaskModalProps = {
+  onClose: () => void;
+};
+
+function AddTaskModal({ onClose }: AddTaskModalProps) {
+  const closeModal = () => {
+    onClose();
+  };
+  return (
+    <div className="addTaskModal">
+      <button
+        type="button"
+        className="addTaskModal__close"
+        onClick={closeModal}
+      >
+        X
+      </button>
+      <h2 className="addTaskModal__title">Précisez votre tâche</h2>
+      <input type="text" className="addTaskModal__input" />
+      <button type="button" className="addTaskModal__button">
+        Valider
+      </button>
+    </div>
+  );
 }
 
 export default AddTaskModal;
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
+import AddTaskModal from '../AddTaskModal/AddTaskModal';
 import './App.scss';
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const showModal = () => {
+    setIsActive(true);
+  };
+
+  const closeModal = () => {
+    setIsActive(false);
+  };
   return (
     <div className="app">
       <h1 className="app__title">TodoList</h1>
-      <button type="button" className="app__button">
+      <button type="button" className="app__button" onClick={showModal}>
         Ajoutez une tâche
       </button>
+      {isActive && <AddTaskModal onClose={closeModal} />}
     </div>
   );
 }
